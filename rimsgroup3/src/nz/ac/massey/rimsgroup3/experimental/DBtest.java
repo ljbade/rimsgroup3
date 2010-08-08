de07bdb8a152c92a43e3d5b4c56c836620d3fd95
goodundead@hotmail.com <goodundead@hotmail.com@95e1605a-49a2-c3d6-b5b2-f7671df82f6b>
2010-08-08 21:33:46 +0000

    PetersServlet replaced with DBtest .. DUnno why it wouldn't work for me

--- rimsgroup3/src/nz/ac/massey/rimsgroup3/experimental/DBtest.java
+++ rimsgroup3/src/nz/ac/massey/rimsgroup3/experimental/DBtest.java
@@ -7,15 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class PetersServlet
+ * Servlet implementation class DBtest
  */
-public class PetersServlet extends HttpServlet {
+public class DBtest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public PetersServlet() {
+    public DBtest() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -23,7 +23,7 @@ public class PetersServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().print("<html><head><title>Test Page</title><body><h1>Hello Peter!</h1></body></html>");
 	}
 
