28f48847567bd21634d9c662d4a154fa63cf5c17
leithbade <leithbade@95e1605a-49a2-c3d6-b5b2-f7671df82f6b>
2010-08-10 02:51:59 +0000

    Updated CrossrefRetriever to return real Crossref results based on Steve's code. Added a test for CrossrefRetriever.

---
+++ trunk/rimsgroup3/test/nz/ac/massey/rimsgroup3/metadata/plugin/test/CrossrefRetrieverTest.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package nz.ac.massey.rimsgroup3.metadata.plugin.test;
+
+import java.util.ArrayList;
+
+import nz.ac.massey.rimsgroup3.metadata.bean.Journal;
+import nz.ac.massey.rimsgroup3.metadata.plugin.CrossrefRetriever;
+import junit.framework.TestCase;
+
+/**
+* @author Leith
+*
+*/
+public class CrossrefRetrieverTest extends TestCase {
+	CrossrefRetriever retriever;
+	
+	public void setUp() {
+		retriever = new CrossrefRetriever();
+	}
+	
+	public void testJournalDoi() {
+		//String doi = "10.3998/3336451.0009.101";
+		String doi = "10.1109/79.952804";
+		Journal expected = new Journal();
+		expected.setJournalTitle("Journal of Electronic Publishing");
+		expected.setIssnIsbn("10802711");
+		expected.setVolume(9);
+		expected.setYear(2006);
+		expected.setIssue(1);
+		expected.setArticleTitle("In Google We Trust?");
+		// TODO need to test other fields too!
+		
+		Journal result = (Journal) retriever.retrievePublication(doi);
+		
+		assertTrue(result != null);
+		
+		assertTrue(result.getJournalTitle().equals(expected.getJournalTitle()));
+		assertTrue(result.getIssnIsbn().equals(expected.getIssnIsbn()));
+		assertTrue(result.getVolume() == expected.getVolume());
+		assertTrue(result.getYear() == expected.getYear());
+		assertTrue(result.getIssue() == expected.getIssue());
+		assertTrue(result.getArticleTitle().equals(expected.getArticleTitle()));
+	}
+}
---
+++ trunk/rimsgroup3/test/nz/ac/massey/rimsgroup3/metadata/test/MetadataRetrieverFactoryTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package nz.ac.massey.rimsgroup3.metadata.test;
+
+import java.util.ArrayList;
+
+import nz.ac.massey.rimsgroup3.metadata.MetadataRetrieverFactory;
+import nz.ac.massey.rimsgroup3.metadata.bean.Publication;
+import junit.framework.TestCase;
+
+/**
+* @author Leith
+*
+*/
+public class MetadataRetrieverFactoryTest extends TestCase {
+	MetadataRetrieverFactory factory;
+	
+	public void setUp() {
+		factory = MetadataRetrieverFactory.get();
+	}
+	
+	// TODO: change this to use a mock plugin?
+	public void testFindDoi() {
+		ArrayList<String> dois = new ArrayList<String>();
+		dois.add("crossref");
+		dois.add("scopus");
+		
+		for (String doi : dois)	{
+			Publication publication = factory.retrievePublication(doi);
+			
+			assertTrue(publication != null);
+		}
+	}
+}
