6fd8a982e4cbf841556f3d754ddd6779c0d0c5a7
leithbade <leithbade@95e1605a-49a2-c3d6-b5b2-f7671df82f6b>
2010-10-10 03:53:31 +0000

    Removed some warnings.

--- rimsgroup3/src/nz/ac/massey/rimsgroup3/database/ReadStatements.java
+++ rimsgroup3/src/nz/ac/massey/rimsgroup3/database/ReadStatements.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
 
 import nz.ac.massey.rimsgroup3.metadata.bean.*;
 
