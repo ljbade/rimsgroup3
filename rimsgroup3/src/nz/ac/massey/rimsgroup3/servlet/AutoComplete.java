e211d98ce7ff007542050f7f3043061f2d68c79f
goodundead@hotmail.com <goodundead@hotmail.com@95e1605a-49a2-c3d6-b5b2-f7671df82f6b>
2010-10-13 07:27:37 +0000

    AutoComplete made slightly more robust, + test case proving it , Ready Steady Cook

--- rimsgroup3/src/nz/ac/massey/rimsgroup3/servlet/AutoComplete.java
+++ rimsgroup3/src/nz/ac/massey/rimsgroup3/servlet/AutoComplete.java
@@ -34,62 +34,64 @@ public class AutoComplete extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
 		String targetId = request.getParameter("id");
 		boolean publisherFound = false;
-	    if (targetId != null) 
+	    if (targetId == null || targetId.equals("")) 
 	    {
-	    	targetId = targetId.trim().toLowerCase();
+	    	response.setStatus(HttpServletResponse.SC_NO_CONTENT); 
 	    }
-	    String path = getClass().getClassLoader().getResource("../../.").getPath();
-    	path = path.replace("%20", " ");
-    	String fs = System.getProperty("file.separator");
-    	try 
-    	{
-    		InputStream publisherText = new FileInputStream(path + "publishers" + fs + "publishers.txt");
-    		BufferedReader reading = new BufferedReader(new InputStreamReader(publisherText));
-    		String line = null;
-    		StringTokenizer tokenizer;
-    		StringBuffer stringbuff = new StringBuffer();
-    		while ((line = reading.readLine()) != null)
-    		{
-    			tokenizer = new StringTokenizer(line, ",");
-    			while(tokenizer.hasMoreTokens())
-    			{
-    				String token = tokenizer.nextToken();
-    				if (token.toLowerCase().startsWith(targetId))
-    				{
-    					
-    					 stringbuff.append("<publisher>"); 
-    					 stringbuff.append("<name>");
-    		             stringbuff.append(token);
-    		             stringbuff.append("</name>");
-    		             stringbuff.append("</publisher>"); 
-    		             publisherFound = true;
-    				}
-    			}
-    		}
-    		publisherText.close();
-    		if (publisherFound == true) { 
- 	           response.setContentType("text/xml"); 
- 	           response.setHeader("Cache-Control", "no-cache"); 
- 	           response.getWriter().write("<publishers>" + 
- 	                   stringbuff.toString() + "</publishers>"); 
- 	           out.close();
- 	       } 
-    		else { 
- 	           response.setStatus(HttpServletResponse.SC_NO_CONTENT); 
-    		}
-    	}
-    	catch (Exception e)
-        {
-    		 servletCtx.log("Failed to access Publisher.txt", e);
-    		 response.setStatus(HttpServletResponse.SC_NO_CONTENT); 
-        }
-     
-	
-    	
+	    else
+	    {	
+	    	targetId = targetId.toLowerCase().trim();
+	    	String path = getClass().getClassLoader().getResource("../../.").getPath();
+	    	path = path.replace("%20", " ");
+	    	String fs = System.getProperty("file.separator");
+    	 
+	    	try 
+	    	{
+	    		InputStream publisherText = new FileInputStream(path + "publishers" + fs + "publishers.txt");
+	    		BufferedReader reading = new BufferedReader(new InputStreamReader(publisherText));
+	    		String line = null;
+	    		StringTokenizer tokenizer;
+	    		StringBuffer stringbuff = new StringBuffer();
+	    		while ((line = reading.readLine()) != null)
+	    		{
+	    			tokenizer = new StringTokenizer(line, ",");
+	    			while(tokenizer.hasMoreTokens())
+	    			{
+	    				String token = tokenizer.nextToken();
+	    				if (token.toLowerCase().startsWith(targetId))
+	    				{
+	    					stringbuff.append("<publisher>"); 
+	    					stringbuff.append("<name>");
+	    					stringbuff.append(token);
+	    					stringbuff.append("</name>");
+	    					stringbuff.append("</publisher>"); 
+	    					publisherFound = true;
+	    				}
+	    			}
+	    		}
+	    		publisherText.close();
+	    		if (publisherFound == true)
+	    		{ 
+	    			response.setContentType("text/xml"); 
+	    			response.setHeader("Cache-Control", "no-cache"); 
+	    			response.getWriter().write("<publishers>" + stringbuff.toString() + "</publishers>"); 
+ 	                out.close();
+	    		} 
+	    		else
+	    		{ 
+	    			response.setStatus(HttpServletResponse.SC_NO_CONTENT); 
+	    		}
+	    	}
+	    	catch (Exception e)
+	    	{
+	    		servletCtx.log("Failed to access Publisher.txt", e);
+	    		response.setStatus(HttpServletResponse.SC_NO_CONTENT); 
+	    	}
+	    }
 	}
 
 	/**
