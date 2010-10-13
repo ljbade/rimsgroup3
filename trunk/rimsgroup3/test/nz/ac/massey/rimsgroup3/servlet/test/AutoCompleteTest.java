bc9e634dcedf557dd8fe4d9af78e0c2a8bc9218c
goodundead@hotmail.com <goodundead@hotmail.com@95e1605a-49a2-c3d6-b5b2-f7671df82f6b>
2010-10-13 07:27:37 +0000

    AutoComplete made slightly more robust, + test case proving it , Ready Steady Cook

---
+++ trunk/rimsgroup3/test/nz/ac/massey/rimsgroup3/servlet/test/AutoCompleteTest.java
@@ -0,0 +1,63 @@
+package nz.ac.massey.rimsgroup3.servlet.test;
+
+
+import java.io.IOException;
+
+import	 nz.ac.massey.rimsgroup3.servlet.*;
+
+
+import javax.naming.Context; 
+import javax.naming.InitialContext;  
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;  
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.sql.DataSource;  
+import org.apache.cactus.*;  
+import junit.framework.*; 
+
+
+public class AutoCompleteTest extends ServletTestCase  {
+
+	
+	public AutoCompleteTest (String name){
+		super(name);
+		System.setProperty("cactus.contextURL", "http://localhost:8080/rimsgroup3");
+	}
+	
+	public void testSAutoComplete() throws ServletException, IOException{
+		String input = "s";	
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/AutoComplete?id=" + input); 
+		dispatcher.forward(request, response); 
+	}
+	
+	public void endSAutoComplete(WebResponse response) {
+		String output = response.getText();
+		assertEquals("<publishers><publisher><name>SpringerLink</name></publisher></publishers>",output);
+	}
+	
+	
+	public void testIAutoComplete() throws ServletException, IOException{
+		String input = "i";	
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/AutoComplete?id=" + input); 
+		dispatcher.forward(request, response); 
+	}
+	
+	public void endIAutoComplete(WebResponse response) {
+		String output = response.getText();
+		assertEquals("<publishers><publisher><name>IEEE</name></publisher><publisher><name>IWA Publishing</name></publisher></publishers>",output);
+	}
+	
+	public void testNoAutoComplete() throws ServletException, IOException{
+		String input = "";	
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/AutoComplete?id=" + input); 
+		dispatcher.forward(request, response); 
+	}
+	
+	public void endNoAutoComplete(WebResponse response) {
+		String output = response.getText();
+		assertEquals("",output);
+	}
+	
+	
+}
