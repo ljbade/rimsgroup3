3c08f2977da1f3095d40507ed3e89dfffe69963d
eskimobob11 <eskimobob11@95e1605a-49a2-c3d6-b5b2-f7671df82f6b>
2010-10-13 03:33:01 +0000

    Overwrote changes that leith committed before, put them back in with my changes as well.

--- trunk/rimsgroup3/src/nz/ac/massey/rimsgroup3/metadata/plugin/ScopusRetriever.java
+++ trunk/rimsgroup3/src/nz/ac/massey/rimsgroup3/metadata/plugin/ScopusRetriever.java
@@ -43,33 +43,38 @@ public class ScopusRetriever implements MetadataRetriever {
 	@Override
 	public Publication retrievePublication(String doi) {
 		String json = performQuery(doi);
-		Publication publication = null;
-		if(json!=null){
-			publication = decodeJson(json);
+		if(json == null){
+		return null;
 		}
+		Publication publication = decodeJson(json);
+		
 		return publication;
 	}
 	
 	private Publication decodeJson(String json)
 	{
-		JsonResult result= null;
+		if (json.isEmpty()) {
+			return null;
+		}
+		
 		json = json.substring(json.indexOf("(") + 1, json.lastIndexOf(")"));
+		
+		if (json.isEmpty()) {
+			return null;
+		}
 		
 		Gson gson = new Gson();
 		
 		JsonObject jsonObj = gson.fromJson(json, JsonObject.class);
-		try{
-			result = jsonObj.PartOK.Results[0];
-		}
-		catch(IndexOutOfBoundsException e){
+		if (jsonObj == null || jsonObj.PartOK == null || jsonObj.PartOK.Results.length == 0)
+		{
 			return null;
 		}
-		catch(NullPointerException e){
-			return null;
-		}
+		JsonResult result = jsonObj.PartOK.Results[0];
 		
-		if (!result.equals("ar") && !result.equals("ip") && !result.equals("bz"))
-			return null;
+		if (!result.doctype.equals("ar") && !result.doctype.equals("ip") && !result.doctype.equals("bz")) {
+			return null;	
+		}
 		
 		Journal journal = new Journal();
 		journal.setAbstractText(StringEscapeUtils.unescapeHtml(result.abstractString));
@@ -87,17 +92,38 @@ public class ScopusRetriever implements MetadataRetriever {
 		journal.setUrl(StringEscapeUtils.unescapeHtml(result.inwardurl));
 		journal.setVolume(StringEscapeUtils.unescapeHtml(result.vol));
 		
-		if (result.firstauth != null)
-		{
-			ArrayList<Author> authors = new ArrayList<Author>();
-			Author author = new Author();
-			author.setFirstName(result.firstauth.split(", ")[0]);
-			author.setLastName(result.firstauth.split(", ")[1]);
-			author.setAffiliation(result.affiliations);
+
+		ArrayList<Author> authors = new ArrayList<Author>();
+		Author author = new Author();
+		
+		if (result.firstauth != null) {
+			String[] parts = result.firstauth.split(", ");
 			
-			authors.add(author);
-			journal.setAuthors(authors);
+			if (parts.length < 2) {
+				author.setFirstName("");
+				author.setMiddleName("");
+				author.setLastName("");
+				author.setAffiliation("");
+			} else if (parts.length < 3) {
+				author.setFirstName(parts[0]);
+				author.setMiddleName("");
+				author.setLastName(parts[1]);
+			} else {
+				author.setFirstName(parts[0]);
+				author.setMiddleName(parts[1]);
+				author.setLastName(parts[parts.length - 1]);
+			}
+			
+			author.setAffiliation(result.affiliations);
+		} else {
+			author.setFirstName("");
+			author.setMiddleName("");
+			author.setLastName("");
+			author.setAffiliation("");
 		}
+		
+		authors.add(author);
+		journal.setAuthors(authors);
 		
 		return journal;
 	}
