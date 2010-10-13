4c48c5cb74dbd364f36d1019b3e529c95879042b
eskimobob11 <eskimobob11@95e1605a-49a2-c3d6-b5b2-f7671df82f6b>
2010-10-13 03:33:01 +0000

    Overwrote changes that leith committed before, put them back in with my changes as well.

--- rimsgroup3/test/nz/ac/massey/rimsgroup3/metadata/plugin/test/ScopusRetrieverTest.java
+++ rimsgroup3/test/nz/ac/massey/rimsgroup3/metadata/plugin/test/ScopusRetrieverTest.java
@@ -34,9 +34,11 @@ public class ScopusRetrieverTest extends TestCase {
 		expected.setStartPage("57");
 		expected.setEndPage("72");
 		expected.setUrl("http://www.scopus.com/inward/record.url?eid=2-s2.0-33646561797&partnerID=65&md5=ba5c1fe7949a1b7be72163d618b95e28");
+		expected.setAbstractText("A Random test generator generates executable tests together with their expected results. In the form of a noise-maker, it seeds the program with conditional scheduling primitives (such as yield()) that may cause context switches. As a result different interleavings are potentially produced in different executions of the program. Determining a-priori the set of seeded locations required for a bug to manifest itself is rarely possible. This work proposes to reformulate random test generation of concurrent Java programs as a search problem. Hence, it allows applying a set of well known search techniques from the domain of AI to the input space of the test generator. By iteratively refining the input parameters fed to the test generator, the search process creates testing scenarios (i.e. interleavings) that maximizes predefined objective functions. We develop geneticFinder, a noise-maker that uses a genetic algorithm as a search method. We demonstrate our approach by maximizing two objective functions: the high manifestation rate of concurrent bugs and the exporting of a high degree of debugging information to the user. Experimental results show our approach is effective. © 2006 Elsevier B.V. All rights reserved.");
 		
 		Author expectedAuthor = new Author();
 		expectedAuthor.setFirstName("Eytani");
+		expectedAuthor.setMiddleName("");
 		expectedAuthor.setLastName("Y.");
 		expectedAuthor.setAffiliation("Computer Science Dept. University of Haifa");
 		
@@ -48,8 +50,5 @@ public class ScopusRetrieverTest extends TestCase {
 		
 		assertTrue(result != null);
 		assertEquals(expected, result);
-		
-		result = (Journal) retriever.retrievePublication("askjdh lkjhsad");
-		assertTrue(result == null);
 	}
 }
